9656e5a27150f6c83c2f3443126c544af70c26e1
Antonio Piazza <antman1p@gmail.com>
2019-04-20 15:38:15 -0400

    still debugging

--- home.js
+++ home.js
@@ -25,6 +25,9 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
+app.use('/resources', express.static(__dirname + '/public'));
+
+
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
