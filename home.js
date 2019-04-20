5a85bf02257c274ee4011014e23e08032bbb7057
Antonio Piazza <antman1p@gmail.com>
2019-04-20 15:02:28 -0400

    debugging path stuff for graphFile.json

--- home.js
+++ home.js
@@ -25,7 +25,7 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
