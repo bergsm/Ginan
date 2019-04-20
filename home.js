529eef6835f576997934afdde0523d53f852f0b3
Antonio Piazza <antman1p@gmail.com>
2019-04-20 15:24:21 -0400

    Still debugging

--- home.js
+++ home.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-app.use(express.static(__dirname + 'public'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
