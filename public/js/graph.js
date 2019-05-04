8fcad426c00e2b97180e38eda35787c48acfe292
Antonio Piazza <antman1p@gmail.com>
2019-05-04 13:33:35 -0400

    messing with marker viewbox

--- public/js/graph.js
+++ public/js/graph.js
@@ -9,7 +9,7 @@ var svg = d3.select("svg"),
 svg.append('defs').append('marker')
     .attrs({
         'id': 'arrowhead',
-        'viewBox': '-0 -5 10 10',
+        'viewBox': '-0 -5 20 20',
         'refX': 13,
         'refY': 0,
         'orient': 'auto',
