6e9028c51ade5608762fa8e13c79303ea791bde8
Antonio Piazza <antman1p@gmail.com>
2019-05-04 12:57:28 -0400

    experimenting with path length

--- public/js/graph.js
+++ public/js/graph.js
@@ -102,7 +102,7 @@ function ticked() {
     node
         .attr("transform", function (d) { return "translate(" + d.x + ", " + d.y + ")"; });
     edgepaths.attr('d', function (d) {
-        return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + (d.target.x - 20) + ' ' + (d.target.y - 20);
+        return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + (d.target.x - 100) + ' ' + (d.target.y - 100);
     });
     edgelabels.attr('transform', function (d) {
         if (d.target.x < d.source.x) {
