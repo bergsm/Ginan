312cec2a8a81937db420463771aa1f64f2d7dffc
Antonio Piazza <antman1p@gmail.com>
2019-05-05 11:51:07 -0400

    debug post

--- home.js
+++ home.js
@@ -27,7 +27,7 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-app.post('/test', (req,res) => {
+app.post('/test', function(req,res) {
     consloe.log("Should Show shit below");
     console.log(req.body);
     res.send("recieved your request!");
