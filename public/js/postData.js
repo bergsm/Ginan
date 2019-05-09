567a4258b39e3adc50ed32ab14504122220e4ebf
Antonio Piazza <antman1p@gmail.com>
2019-05-09 13:30:39 -0400

    Seperated postData.js from handlebars file

---
+++ public/js/postData.js
@@ -0,0 +1,29 @@
+$('#submitButton').click(function () {
+    //alert('clicked')
+    $.ajax({
+        url: "/test-page",
+        type: "POST",
+        dataType: 'json',
+        data: JSON.stringify({
+            starting_url: $('#inputURL').val(),
+            crawl_limit: $('#crawlLimit').val(),
+            search_type: $("input[name='data[searchType]']:checked").val()
+        }),
+        contentType: "application/json;charset=UTF-8",
+        cache: false,
+        timeout: 5000,
+        complete: function () {
+            //called when complete
+            console.log('process complete');
+        },
+
+        success: function (data) {
+            console.log(data);
+            console.log('process sucess');
+        },
+
+        error: function () {
+            console.log('process error');
+        },
+    });
+});
