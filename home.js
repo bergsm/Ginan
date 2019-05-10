522e4957fafbca341351332527520eef0c0b521c
Antonio Piazza <antman1p@gmail.com>
2019-05-10 17:25:39 -0400

    Test espn links

--- home.js
+++ home.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3030);
+app.set('port', 50500);
 
 app.get('/', function(req,res){
   res.render('crawler');
