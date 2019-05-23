1053904526051f71f3962fcfd010e2e4804da163
Antonio Piazza <antman1p@gmail.com>
2019-05-22 20:29:28 -0400

    debugging cookie data pull

--- public/js/graph.js
+++ public/js/graph.js
@@ -29,7 +29,6 @@ var simulation = d3.forceSimulation()
 
 // Test json data in a variable
 var graphtext = '{"nodes": [{"name": "URL","label": " http://www.espn.com","id": 1},{"name": "URL","label": " http://twitter.com/intent/tweet?url=http%3A%2f%2fwww.espn.com%2fespn%2ffeature%2fstory%2f_%2fid%2f18332398%2fespn%2dfantasy%2dapp%2despn&text=Download+the+ESPN+Fantasy+App","id": 2},{"name": "URL","label": " https://twitter.com/signup?context=webintent","id": 3},{"name": "URL","label": " https://twitter.com/signup?context=webintent","id": 4}],"links": [{"source": 1,"target": 2,"type": "Links_To"},{"source": 2,"target": 3,"type": "Links_To"},{"source": 3,"target": 4,"type": "Links_To"}]}';
-//var graphjson = JSON.parse(graphtext);
 
 //Create a cookie test using the variable
 $.cookie('graph_session', graphtext);
@@ -37,8 +36,7 @@ $.cookie('graph_session', graphtext);
 console.log($.cookie('graph_session'));
 
 if ($.cookie('graph_session')) {
-    var root = $.parseJSON($.cookie('graph_session'));
-    d3.json(root, function (error, graph) {
+    d3.json($.cookie('graph-session'), function (error, graph) {
         if (error) throw error;
         update(graph.links, graph.nodes);
    
