5e68dc45846f22d7b4379bdfcc53b4d4755e2ce8
Shawn Berg <bergshawnm@gmail.com>
2019-06-01 14:44:10 -0500

    Printing json to stdout

--- public/DFT.py
+++ public/DFT.py
@@ -6,9 +6,6 @@ from bs4 import BeautifulSoup
 from urlparse import urljoin
 
 outputFP = './public/graphFile.json'
-#cookieName = 'graph_session'
-cookieName = 'test2'
-
 
 # Function to write urls from DFS to json file for
 # d3
@@ -32,7 +29,7 @@ def writeToFile(urls, filePath):
 # Function to write urls from DFS to json cookie for d3
 # Args: urls in order from DFS, cookie name
 # Returns: None
-def writeToCookie(urls, cookieName):
+def writeToCookie(urls):
     jsonString = ''
     jsonString += '{\n  \"nodes\": ['
     for counter, url in enumerate(urls):
@@ -45,15 +42,7 @@ def writeToCookie(urls, cookieName):
         if i < len(urls)-1:
             jsonString += ','
     jsonString += '\n  ]\n}'
-    c = Cookie.SimpleCookie()
-    c[cookieName] = jsonString
-    s = requests.Session()
-    cookie_obj = requests.cookies.create_cookie(domain='localhost:3030',name='test_cookie',value='wish this would work')
-    s.cookies.set_cookie(cookie_obj)
-    #s.post('http://localhost:3030', cookies=c)
-    #s.cookies.set(c)
-    #r = requests.post('http://localhost:3030', cookies=c)
-    #print c.js_output()
+    print(jsonString)
 
 
 # Function to parse page for all links
@@ -61,25 +50,25 @@ def writeToCookie(urls, cookieName):
 # Returns: array/list of links
 def parsePage(url, links):
     #fetch page
-    print("Fetching page..")
+    sys.stderr.write("Fetching page..\n")
     try:
         r = requests.get(url)
         page = r.content
-        print("Parsing..")
+        sys.stderr.write("Parsing..\n")
         soup = BeautifulSoup(page, "html.parser")
         if not soup.body:
-            raise Exception("Page not parsed")
+            raise Exception("Page not parsed\n")
         links = []
         for link in soup.find_all('a'):
             links.append(urljoin(url, link.get('href')))
     except Exception as e:
         #pick another link if available
-        print("Error: " + str(e))
+        sys.stderr.write("Error: " + str(e))
         if len(links) < 1:
-            print("No other links available.")
+            sys.stderr.write("No other links available.\n")
             return []
         else:
-            print("Choosing a different link..")
+            sys.stderr.write("Choosing a different link..\n")
             randNum = random.randint(0, len(links)-1)
             url = links[randNum]
             del links[randNum]
@@ -103,29 +92,26 @@ def DFT(url, depth, urlChain, links, keyword):
     #base case
     if depth == 0 or links == [] or (any(keyword in link for link in urlChain) and keyword):
         #write urlChain to file for graph and exit
-        print("Depth reached, keyword found, no links on page, or url format incorrect. Generating results..")
+        sys.stderr.write("Depth reached, keyword found, no links on page, or url format incorrect. Generating results..\n")
         writeToFile(urlChain, outputFP)
-        writeToCookie(urlChain, cookieName)
+        writeToCookie(urlChain)
         return
 
     #choose one link at random
-    print("Choosing link at random..")
+    sys.stderr.write("Choosing link at random..\n")
     randNum = random.randint(0, len(links)-1)
     url = links[randNum]
     del links[randNum]
-    print("Random url is: " + url)
+    sys.stderr.write("Random url is: " + url + "\n")
 
     #decrement and recurse
     depth -= 1
     DFT(url, depth, urlChain, links, keyword)
 
 def main():
-    c = Cookie.SimpleCookie()
-    c['test'] = 'hello'
-    print(c)
     #Take starting url as argument?
     if len(sys.argv) < 3:
-        print("No url and/or depth argument provided")
+        sys.stderr.write("No url and/or depth argument provided\n")
         exit(1)
 
     #Initialize list of variables
@@ -135,7 +121,7 @@ def main():
     urlChain = []
     links = []
 
-    print("keyword is: " + keyword)
+    sys.stderr.write("keyword is: " + keyword + "\n")
 
     DFT(initUrl, depth, urlChain, links, keyword)
 
