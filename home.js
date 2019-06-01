f09e52e6450f5c43d46f62a6d2054eb618a3d800
Shawn Berg <bergshawnm@gmail.com>
2019-06-01 14:25:15 -0500

    Python stdout saved to cookie

--- home.js
+++ home.js
@@ -29,25 +29,25 @@ app.use(function (req, res, next) {
         var randomNumber=Math.random().toString();
         randomNumber=randomNumber.substring(2,randomNumber.length);
         //read the file synchronously. Was having trouble with async... source: https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
-	var graph_session_data = fs.readFileSync('./public/graph_session', 'utf8'); //will need to change this, I was writing to a file graph_session in BFS script
+	//var graph_session_data = fs.readFileSync('./public/graph_session', 'utf8'); //will need to change this, I was writing to a file graph_session in BFS script
 	//was the data read correctly?
-	console.log(graph_session_data);
+	//console.log(graph_session_data);
 	//set the cookie
-        res.cookie('graph_session',randomNumber, { maxAge: 900000, httpOnly: false });
-        console.log('cookie created successfully');
-	console.log(req.cookies.graph_session);
-     } 
+        //res.cookie('test2',randomNumber, { maxAge: 900000, httpOnly: false });
+        //console.log('cookie created successfully');
+	//console.log(req.cookies.graph_session);
+     }
      else
      {
-     	// yes, cookie was already present 
-        console.log('cookie exists', cookie);
-     } 
+     	// yes, cookie was already present
+        //console.log('cookie exists', cookie);
+     }
      next();
 });
 
 app.get('/', function(req,res){
   //log the cookie data. Source: https://stackoverflow.com/questions/39615479/cookies-undefined-on-node-js-express-localhost
-  console.log(req.cookies.graph_session);
+  //console.log(req.cookies.graph_session);
   res.render('crawler');
 });
 
@@ -56,6 +56,8 @@ app.post('/test-page', function (req, res) {
     //res.send(req.body);
     if (req.body.search_type == 'DFS') {
         const pythonProcess = spawn('python',["./public/DFT.py", req.body.starting_url, req.body.crawl_limit]);
+        //console.log(pythonProcess.stdout.toString());
+        res.cookie('test', pythonProcess.stdout.toString());
         res.render('crawler');
     }
     else {
